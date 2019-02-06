c4aff3d8c66ab4ef436d065aabe8bc5a4f529124
OmarDali <46767715+OmarDali@users.noreply.github.com>
2019-02-05 23:02:04 -0500

    Week 3

---
+++ Circumference.cpp
@@ -0,0 +1,24 @@
+// Circumference.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+double calculateCircumference(double radius);
+const double pi = 3.14;
+
+
+int main()
+{
+	cout << "Please enter radius" << endl;
+	double radius;
+	cin >> radius;
+	cout << calculateCircumference(radius);
+	return 0;
+}
+
+double calculateCircumference(double radius) {
+	double circumference = 2 * pi*radius;
+	return circumference;
+}

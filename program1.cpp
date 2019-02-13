c702c36b7aa0dbe4e790ea01137f767e8ae35c39
OmarDali <46767715+OmarDali@users.noreply.github.com>
2019-02-13 16:01:59 -0500

    Week 4

---
+++ program1.cpp
@@ -0,0 +1,30 @@
+
+#include "pch.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int arrayNum[10] = {}, i;
+	float sum = 0.0, mean;
+
+	for (i = 0; i < 10; i++) {
+
+		cout << "Original Numbers: " << endl;
+		cin >> arrayNum[i];
+		sum += arrayNum[i];
+	}
+	mean = sum / i;
+	cout << "Mean is: " << mean << endl;
+
+	for (i = 0; i < 10; i++) {
+
+		arrayNum[i] = pow(arrayNum[i] - mean, 2);
+		cout << "New Number: " << arrayNum[i] << endl;
+		sum += arrayNum[i];
+	}
+	mean = sum / i;
+	cout << "New mean is: " << mean << endl;
+	cout << "The square root is: " << sqrt(mean) << endl;
+}
+
---
+++ programe2.cpp
@@ -0,0 +1,21 @@
+
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char name[30][164];
+
+	for (int i = 0; i < 30; i++)
+	{
+		cout << "Please enter the students name: " << endl;
+		cin.getline(name[i], 20);
+	}
+
+	for (int i = 0; i < 30; i++)
+	{
+		cout << "Class List: " << name[i] << endl;
+	}
+}
